f207a418c3f23cefa639840b494e71acd3ed8247
Emma Shiller <emmashiller@gmail.com>
2021-08-06 17:37:47 +0200

    feat:handler error

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
 });
 
 app.use(express.json());
-app.use("/", userRouter);
-app.use("/", cardRouter);
 
 app.use((req, res, next) => {
   req.user = {
@@ -25,6 +23,12 @@ app.use((req, res, next) => {
 
   next();
 });
+
+app.use("/", userRouter);
+app.use("/", cardRouter);
+router.use((req, res) => {
+  res.status(404).send({message: "Роутер не найден"});
+}
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
